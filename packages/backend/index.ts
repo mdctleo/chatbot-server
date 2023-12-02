a9b543ff64d60d2fbb6efc31010348cbad2b3bc5
taham0 <taha10@purdue.edu>
2023-12-02 12:44:52 -0500

    initialized openai api
    first commit
    
    Update README.md
    
    initialized openai api
    
    dependencies
    
    change: comments

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -4,10 +4,18 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { CustomWebSocketServer } from './CustomWebSocketServer';
 import path from 'path';
+import { OpenAI } from "langchain/llms/openai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
 
 // Initialize the express engine
 const app: express.Application = express();
 dotenv.config();
+
+const llm = new OpenAI({
+    temperature: 0.9,
+});
+
+const chatModel = new ChatOpenAI();
 
 if (process.env.NODE_ENV !== 'production') {
     app.use(cors());
@@ -34,3 +42,24 @@ const httpServer = app.listen(port, () => {
 });
 
 const cwss = new CustomWebSocketServer({ server: httpServer });
+
+cwss.on('connection', (ws) => {
+    console.log('a new client connected!')
+
+    ws.on('message', async (message) => {
+        // parse json message        
+        const req = JSON.parse(message.toString());
+
+        // handle client requests
+        if (req.type == 'stream') {
+            // call gpt api
+            const text = req.query;
+            const llmResult = await llm.predict(text);
+            
+            console.log(llmResult);
+
+            // send response
+            // ws.send(JSON.stringify(llmResult));
+        }
+    });
+});
